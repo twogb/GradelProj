22010864dcb062eb2c5e33addfe7a9ef0b67e548
twogb <92275665+twogb@users.noreply.github.com>
2023-12-13 10:16:10 +0200

    remove .idea & rewrite Human class with getters and setters & remove jar

--- src/main/java/org/example/HumanClass.java
+++ src/main/java/org/example/HumanClass.java
@@ -1,6 +1,32 @@
 package org.example;
 
 public class HumanClass {
-    String name = "Svitlana";
-    String lastName = "Dubynka";
+    private String name;
+    private String lastName;
+
+    public HumanClass(String name, String lastName) {
+        this.name = name;
+        this.lastName = lastName;
+    }
+
+    public HumanClass() {
+    }
+
+    public HumanClass setLastName(String lastName) {
+        this.lastName = lastName;
+        return this;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public HumanClass setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,9 +3,9 @@ import com.google.gson.Gson;
 
 public class Main {
     public static void main(String[] args){
-            HumanClass myObject = new HumanClass();
-            Gson gson = new Gson();
-            String json = gson.toJson(myObject);
+            HumanClass myObject = new HumanClass("Svitlana", "Dubynka");
+
+            String json = new Gson().toJson(myObject);
             System.out.println(json);
     }
 }
